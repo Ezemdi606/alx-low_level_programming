0bca9b682f53d503e200399f1b2b12051799cba2
root <laurelbenson335@gmail.com>
2022-06-30 14:03:34 -0700

    My first clode

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void)
-	/* The function used here prints the string without having to use a newline character */
+	/* puts automatically adds a newline character */
 {
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
