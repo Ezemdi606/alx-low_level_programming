32868767996d3b1f1dbbc0f0c0b8f6dd49e69ef9
root <laurelbenson335@gmail.com>
2022-06-30 20:06:46 -0700

    Used the write function to print out a script

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Prints out a string using the write function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19);
+	return (1);
+}
