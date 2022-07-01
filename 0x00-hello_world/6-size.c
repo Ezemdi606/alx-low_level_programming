34364e27a5bf7b84471e6a8da708a6d946215ee2
root <laurelbenson335@gmail.com>
2022-06-30 19:53:34 -0700

    Created a script which prints the various types a computer is compiled and run on

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints the size of various types on the computer it is compiled and run on
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", size of (char));
+	printf("Size of an int: %d byte(s)\n", size of (int));
+	printf("Size of a long int: %d byte(s)\n", size of (long int));
+	printf("Size of a long long int: %d byte(s)\n", size of (long long int));
+	printf("Size of a float: %d byte(s)\n", size of (float));
+	return (0);
+
+}
