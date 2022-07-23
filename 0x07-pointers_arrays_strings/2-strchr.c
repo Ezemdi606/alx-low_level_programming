e2a39e27f99c3fefdc752fa4d20f2a841f9f6ca2
root <laurelbenson335@gmail.com>
2022-07-23 05:42:50 -0700

    Demonstrates how to use the memcpy funtion

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#include <string.h>
+/**
+ * main - The main function
+ *
+ * str1 - The destination array
+ *
+ * str12 - The source array
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	char str1[] = "My first program demonstrating memcpy";
+	char str2[] = "Copying to the first array";
+	/**
+	 * seven characters will be copied from
+	 * str2 to str1
+	 */
+	memcpy(str1, str2, 7);
+	printf("%s\n", str1);
+	return (0);
+}
+
+
